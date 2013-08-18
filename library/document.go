6d02dba0477221dc8f0899cce251640a82cf9569
gareth stokes <gareth@betechnology.com.au>
2013-08-18 13:43:13 +1000

    add microseconds to the delta and now divide them

--- library/document.go
+++ library/document.go
@@ -15,7 +15,7 @@ type Document struct {
 
 func (d * Document) expired() bool {
   now := time.Now().UTC().Unix()
-  delta := d.ExpirationDelta / 1000000000
+  delta := d.ExpirationDelta + 1000000000
   expiry := d.DateCreated + int64(delta)
 
   fmt.Printf("now: %d, expiry: %d, created: %d\n", now, expiry, d.DateCreated)
