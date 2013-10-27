c637be4b9511fc13b65fb9bd9c74417b52136477
gareth stokes <gareth@betechnology.com.au>
2013-10-27 18:11:51 +1100

    log value in calls to the cache

--- cache/cache.go
+++ cache/cache.go
@@ -6,12 +6,12 @@ var db = make( map[string] interface{} )
 
 func Get(namespace string, key string) interface{} {
   key = namespace + "." + key
-  fmt.Printf( "cache.Get :: %s\n", key )
+  fmt.Printf( "cache.Get :: %s :: %@\n", key, db[key])
   return db[key]
 }
 
 func Set(namespace string, key string, value interface{}) {
   key = namespace + "." + key
-  fmt.Printf( "cache.Set :: %s\n", key )
+  fmt.Printf( "cache.Set :: %s :: %@\n", key, value )
   db[key] = value
 }
