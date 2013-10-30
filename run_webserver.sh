046dc3a97a626d96af28af0259d836cecef4ba98
gareth stokes <gareth@betechnology.com.au>
2013-10-30 18:06:33 +1100

    log web to a file

--- run_webserver.sh
+++ run_webserver.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 cd web
+
+echo "...resetting log file"
+rm -f log/web.log
+
 echo "...fetching dependencies"
 go get -v
 
 echo "running webserver"
 echo "================="
-go run *.go
+go run *.go 2>&1 >> log/web.log
