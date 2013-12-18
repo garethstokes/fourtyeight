df46e92d3b23faf0193e1df2f320a19e26d8ac6d
gareth stokes <gareth@betechnology.com.au>
2013-12-18 21:29:10 +1100

    put the email goroutine back in

--- web/controller_personal.go
+++ web/controller_personal.go
@@ -214,7 +214,7 @@ func PersonalController() {
     hash := passwords.Compute( user.Username + params.Password )
     cache.Set("users", hash.Hash, user)
 
-    mail.SendWelcomeEmail(u.Email)
+    go mail.SendWelcomeEmail(u.Email)
 
     ok( ctx, map[string] interface{} {
       "token": hash.Hash,
