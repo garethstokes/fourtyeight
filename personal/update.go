556cb0aa8c4461612c683cb2ca1203e91ed92994
gareth stokes <gareth.stokes@fairfaxmedia.com.au>
2014-02-19 21:44:21 +1100

    update method for personal.Person

---
+++ personal/update.go
@@ -0,0 +1,14 @@
+package personal
+
+import (
+  "labix.org/v2/mgo/bson"
+  "fmt"
+)
+
+func (s * Personal) Update( person * Person ) error {
+  fmt.Printf( "Personal.Update :: %@\n", person )
+
+  err := s.collection.Update(bson.M{"key", person.Key}, person)
+  return err;
+}
+
